21c53d4bf11a8c68dbb7f890f6c7f6200e52ddd4
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-04-26 20:08:20 -0600

    Finished Assignment 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,27 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+ {
+        Random randomGenerator = new Random();
+        int Numberguess = randomGenerator.Next(1, 101);
+        int guess = -1;
+        while (guess != Numberguess)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (Numberguess > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (Numberguess < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
