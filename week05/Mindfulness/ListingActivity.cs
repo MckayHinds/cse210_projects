8c87b7f5df780e1b01ce276c177af45471c15857
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-24 23:00:45 -0600

    Prompt string

---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+public class ListingActivity : Activity
+{
+    private int _count = 0;
+    private List<string> _prompts = new List<string>
+    {
+        "What are your favorite things to do?",
+        "Who do you appreciate in life?",
+        "Who have you helped this week?",
+        "What have you accomplished this week?"
+    };
+    public ListingActivity(string name, string description) : base(name, description) { }
+    
+}
