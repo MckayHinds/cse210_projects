662d2d3dda5ed41cabc15898a7031c5d828a2f2e
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-25 00:47:17 -0600

    ShowSpinner Fix

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -27,5 +27,22 @@ public class Activity
             Console.WriteLine("Invalid input, Please try again. Defaulting in 30 seconds");
             _duration = 30;
         }
+        Console.WriteLine("Starting in 30 Seconds!");
+        ShowCountdown(5);
+    }
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine("Thank you for completing the activity!");
+        ShowSpinner(3);
+    }
+    public void ShowSpinner(int seconds)
+    {
+        string[] spinner = { "|", "/", "-", "\\" };
+        for (int i = seconds; i > 0; i++)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b");
+        }
     }
 }
