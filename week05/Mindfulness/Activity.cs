6cd8712b8ec2c0b5403114e5e06ef9f4370909e3
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-25 00:53:16 -0600

    Show Countdown fix

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -38,11 +38,20 @@ public class Activity
     public void ShowSpinner(int seconds)
     {
         string[] spinner = { "|", "/", "-", "\\" };
-        for (int i = seconds; i > 0; i++)
+        for (int i = 0; i < seconds * 4; i++)
+        {
+            Console.Write(spinner[i % 4]);
+            Thread.Sleep(250);
+            Console.Write("\b");
+        }
+    }
+    public void ShowCountdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
         {
             Console.Write(i);
             Thread.Sleep(1000);
-            Console.Write("\b");
+            Console.Write("\b \b");
         }
     }
 }
