a52962b4f01a602572efe361871e3ca3d6d96e24
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-25 00:27:59 -0600

    Activity Basic code pt 1

---
+++ week05/Mindfulness/Activity.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading;
+
+public class Activity
+{
+    protected string _name;
+    protected string _description;
+    protected int _duration;
+
+    public Activity(string name, string description)
+    {
+        
+    }
+}
