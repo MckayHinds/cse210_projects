9db5acde7d198bd0ee264ae4fcbc2aeb7450710a
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-24 21:14:16 -0600

    Finished Breathing Basics

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -7,5 +7,15 @@ class Program
         Console.WriteLine("Mindfullness Project");
         Console.WriteLine("Please Pick One: 1. Breathing Exersizes 2. Reflection 3. Ponder and List");
         string choice = Console.ReadLine();
+        if (choice == "1") ;
+        {
+            Console.WriteLine("Lets begin our breathing session.");
+            Console.WriteLine("Take a deep breath in.");
+            Console.WriteLine("Hold...");
+            Thread.Sleep(15000);
+            Console.WriteLine("Breathe out.");
+
+        }
     }
+
 }
