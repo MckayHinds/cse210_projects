b097cf9bc336bfa87cb3e767c3268b647a7c95f7
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-25 00:34:26 -0600

    Display Starting Messages and durations

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -9,6 +9,23 @@ public class Activity
 
     public Activity(string name, string description)
     {
-        
+        _name = name;
+        _description = description;
+    }
+    public void DisplayStartingMessage()
+    {
+        Console.Clear();
+        Console.WriteLine($"{_name}: {_description}");
+        Console.Write("Enter your desired Duration in Seconds");
+        string input = Console.ReadLine();
+        try
+        {
+            _duration = int.Parse(input);
+        }
+        catch
+        {
+            Console.WriteLine("Invalid input, Please try again. Defaulting in 30 seconds");
+            _duration = 30;
+        }
     }
 }
