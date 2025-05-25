15dfac3fbec59512253e21f6dc2c7d821db25f69
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-24 22:01:18 -0600

    Duration update

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -5,9 +5,21 @@ public class Activity;
     private string _name;
     private string _description;
     protected interface _duration;
+
     public Activity(string name, string _description);
     {
-    _name = name;
-    _description = description;
+        _name = name;
+        _description = description;
+    }
+    public void DisplayStartingMessage()
+    {
+    Console.Clear();
+    Console.WriteLine($"{_name}: {_description}");
+    Console.WriteLine("Enter desired activity duration in seconds: ");
+    string duration = Console.ReadLine();
+    try
+    {
+
+    }
     }
 }
