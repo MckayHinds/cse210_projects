791f433fb74f445fc6520f72284e577f7fa3d289
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-29 15:16:29 -0600

    Setting up colors

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
     }
 }
