7fd86519d6f4b6fb06651e038ee6170328eec4d8
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-29 15:24:06 -0600

    Square slight fix

--- week06/Shapes/Square.cs
+++ week06/Shapes/Square.cs
@@ -3,7 +3,7 @@ public class Square : Shape
     private float _side;
     public Square(string color, float side) : base(color)
     {
-        side = _side;
+        _side = side;
     }
     public override float GetArea()
     {
