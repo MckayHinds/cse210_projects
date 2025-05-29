84caaeae79e89ffeac53157b50c19cc890218ddd
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-29 15:07:50 -0600

    Rectangle set up - length and width

---
+++ week06/Shapes/Rectangle.cs
@@ -0,0 +1,9 @@
+public class Rectangle : Shape
+{
+    private float _length;
+    private float _width;
+    public Rectangle(string color, float length, float width) : base(color)
+    {
+        
+    }
+}
