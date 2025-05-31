a67d1faaabe4ce10afcf3b06e6d65034f3b34e7f
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-31 13:32:57 -0600

    record event, iscomplete and getdetailsstring setup

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -6,6 +6,15 @@ public abstract class Goal
 
     public Goal(string name, string description, int points)
     {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+    // int and bool (let's hope this works like how it should)
+    public abstract int RecordEvent();
+    public abstract bool isComplete();
+    public virtual string GetDetailsString()
+    {
         
     }
 }
