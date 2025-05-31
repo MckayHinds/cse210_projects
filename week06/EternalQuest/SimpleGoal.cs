22fb8322c227c4d65a90ee08be2bdf8fbb2ab1eb
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-31 13:21:41 -0600

    Simple goal base setup

---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,9 @@
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points) : base(name, description, points)
+    {
+        _isComplete false;
+    }
+}
