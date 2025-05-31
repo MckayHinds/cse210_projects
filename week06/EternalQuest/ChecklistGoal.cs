9c97ec791d841ab238ede0099c8fc38f9615a6ea
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-31 14:08:26 -0600

    setting up ints as well as getting the base of the code setup

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,11 @@
+public class ChecklistGoal : Goal
+{
+    private int _ammountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, int points, int target, int bonus) : base(name, description, points)
+    {
+        
+    }
+}
