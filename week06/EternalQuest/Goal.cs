4e195d209d2d188fbb3a7ec288a4b13518fa0f81
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-31 13:27:25 -0600

    Setting up goal with strings and such

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,11 @@
+public abstract class Goal
+{
+    protected string _shortName;
+    protected string _description;
+    protected int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        
+    }
+}
