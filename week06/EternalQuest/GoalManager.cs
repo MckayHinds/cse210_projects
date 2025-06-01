b6e115dc41485584233c5ead9630604777ca829b
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-05-31 18:32:29 -0600

    getting list set up

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -4,5 +4,14 @@ using System.IO;
 
 public class GoalManager
 {
-    
+    private List<Goal> _goals = new List<Goal>();
+    private int _score = 0;
+    public void Start()
+    {
+        string input = "";
+        while (input != "7")
+        {
+            
+        }
+    }
 }
