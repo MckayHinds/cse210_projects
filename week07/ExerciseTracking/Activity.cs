ac6b0057069c303544022926880e2587eb8a749f
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-06-02 10:30:44 -0600

    setting up date and time

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -2,4 +2,12 @@ public abstract class Activity
 {
     private DateTime _date;
     private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+    public DateTime Date => date;
+    public int Minutes => _minutes;
 }
