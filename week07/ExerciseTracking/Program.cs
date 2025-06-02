dfab6b1fa238f8e28b37b9843f2c558d0e31d77d
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-06-02 11:40:25 -0600

    Get summary in program.cs

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -11,5 +11,9 @@ class Program
             new Cycling(new DateTime(2025,06,02),45, 15.5),
             new Swimming(new DateTime(2025,06,02),40,30)
         };
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
