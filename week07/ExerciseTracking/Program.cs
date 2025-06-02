8cfdba8367d42834781c555ff651b891d0ba3d41
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-06-02 11:35:57 -0600

    Running, Cycling, and Swimming date/time beginning

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -7,7 +7,9 @@ class Program
     {
         List<Activity> activities = new List<Activity>
         {
-            
-        }
+            new Running()
+            new Cycling()
+            new Swimming()
+        };
     }
 }
