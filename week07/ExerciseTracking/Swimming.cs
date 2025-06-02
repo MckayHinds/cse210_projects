e000fe44589b56d832c3c929b0ed827ce382b5ac
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-06-02 11:26:03 -0600

    Having trouble locating error, will continue to search and code.

--- week07/ExerciseTracking/Swimming.cs
+++ week07/ExerciseTracking/Swimming.cs
@@ -1,4 +1,9 @@
 public class Swimming : Activity
 {
     private int _laps;
+
+    public Swimming(DateTime date, int minutes, double speed) : base(date, minutes)
+    {
+        _laps = laps;
+    }
 }
