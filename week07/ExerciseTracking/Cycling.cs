a06fdade0473a8c3ccc7bfa6044f5e32cafa6dca
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-06-02 11:22:42 -0600

    Math portion Cycling complete

--- week07/ExerciseTracking/Cycling.cs
+++ week07/ExerciseTracking/Cycling.cs
@@ -3,6 +3,10 @@ public class Cycling : Activity
     private double _speed;
     public Cycling(DateTime date, int minutes, double speed) : base(date, minutes)
     {
-        
+        _speed = speed;
     }
+    //Math portion - using "goes to" lambda operator
+    public override double GetSpeed() => _speed;
+    public override double GetDistance() => (_speed * Minutes) / 60;
+    public override double GetPace() => 60 / _speed;
 }
