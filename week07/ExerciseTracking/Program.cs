007f07b5131a11be718fa23595b9d32f2e1ab961
mhinds03.byui.edu <mckay.a.hinds@gmail.com>
2025-06-02 11:38:56 -0600

    Datetime finish

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -7,9 +7,9 @@ class Program
     {
         List<Activity> activities = new List<Activity>
         {
-            new Running()
-            new Cycling()
-            new Swimming()
+            new Running(new DateTime(2025,06,02), 30, 4.8),
+            new Cycling(new DateTime(2025,06,02),45, 15.5),
+            new Swimming(new DateTime(2025,06,02),40,30)
         };
     }
 }
